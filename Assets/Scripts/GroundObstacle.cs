f9f0c403604f2c6ebcf393dc04f0edff1a3534fd
Caiozed <caio.nevesleite@gmail.com>
2018-07-10 21:20:01 -0300

    Adding new assets and effects

---
+++ Assets/Scripts/GroundObstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundObstacle : MonoBehaviour {
+
+ParticleSystem ps;
+	// Use this for initialization
+	void Start () {
+		ps = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Player")){
+			ps.Play();
+			GetComponent<MeshRenderer>().enabled = false;
+			GetComponent<Collider>().enabled = false;
+		}
+	}
+}
